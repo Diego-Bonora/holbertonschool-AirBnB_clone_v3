cbca5f5ee3cd5ae6ab46081cd4629448c49373c5
Diego-Bonora <bonoradi@gmail.com>
2023-08-25 13:30:48 -0300

    view for amenities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,4 @@ app_views = Blueprint('simple_page', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *  # nopep8
 from api.v1.views.states import *  # nopep8
 from api.v1.views.cities import *  # nopep8
+from api.v1.views.amenities import *  # nopep8
