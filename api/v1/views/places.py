bab1252cc527ea7df26d05a872da6d0206e8ee2e
Diego-Bonora <bonoradi@gmail.com>
2023-08-25 14:56:54 -0300

    trict_slaches=False added to methods

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,7 +8,8 @@ from models.city import City
 from models.user import User
 
 
-@app_views.route("/cities/<city_id>/places", methods=["GET"])
+@app_views.route("/cities/<city_id>/places", methods=["GET"],
+                 strict_slashes=False)
 def list_place_from_city(city_id):
     city = storage.get(City, city_id)
     if not city:
@@ -21,7 +22,8 @@ def list_place_from_city(city_id):
     return jsonify(places)
 
 
-@app_views.route("/places/<place_id>", methods=["GET"])
+@app_views.route("/places/<place_id>", methods=["GET"],
+                 strict_slashes=False)
 def get_place(place_id):
     place = storage.get(Place, place_id)
     if not place:
@@ -29,7 +31,8 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route("places/<place_id>", methods=["DELETE"])
+@app_views.route("places/<place_id>", methods=["DELETE"],
+                 strict_slashes=False)
 def delete_place(place_id):
     place = storage.get(Place, place_id)
     if not place:
@@ -39,7 +42,8 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route("cities/<city_id>/places", methods=["POST"])
+@app_views.route("cities/<city_id>/places", methods=["POST"],
+                 strict_slashes=False)
 def create_place(city_id):
     city = storage.get(City, city_id)
     if not city:
@@ -64,7 +68,8 @@ def create_place(city_id):
         return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route("/places/<place_id>", methods=["PUT"])
+@app_views.route("/places/<place_id>", methods=["PUT"],
+                 strict_slashes=False)
 def update_user(place_id):
     """ updates a user """
     json_data = request.get_json()
