0654faec80d2e4875cf955a8541a8aa5308dec89
Diego-Bonora <bonoradi@gmail.com>
2023-08-25 15:29:12 -0300

    no places import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,9 +3,8 @@ from flask import Blueprint
 
 app_views = Blueprint('simple_page', __name__, url_prefix='/api/v1')
 
+from api.v1.views.amenities import *  # nopep8
+from api.v1.views.cities import *  # nopep8
 from api.v1.views.index import *  # nopep8
 from api.v1.views.states import *  # nopep8
-from api.v1.views.cities import *  # nopep8
-from api.v1.views.amenities import *  # nopep8
 from api.v1.views.users import *  # nopep8
-from api.v1.views.places import *  # nopep8
